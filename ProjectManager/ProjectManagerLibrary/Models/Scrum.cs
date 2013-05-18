b5c0dcde5b30591687d88c3bee90cc6f5d01c939
Ben Hejl <ben@benhejll.com>
2013-05-18 09:39:27 -0400

    Add initial project files from Jen

---
+++ ProjectManager/ProjectManagerLibrary/Models/Scrum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectManagerLibrary.Models
+{
+    public class Scrum 
+    {
+        public int UserId { get; set; }
+
+    }
+}
