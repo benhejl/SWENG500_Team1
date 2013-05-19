677dcd79d2df174a6eddc1f3ba892c1d08e1fc3d
Lauren <Laur10513@gmail.com>
2013-05-18 20:38:49 -0400

    Adding the CalendarController class and associated test class.

---
+++ ProjectManager/ProjectManagerWeb/Controllers/CalendarController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProjectManagerLibrary.Models;
+
+namespace ProjectManagerWeb.Controllers
+{
+    public class CalendarController
+    {
+        public bool CreateNewCalendar()
+        {
+            return true;
+        }
+        public bool DeleteExistingCalendar()
+        {
+            return true;
+        }
+        public bool AddEventToCalendar()
+        {
+            return true;
+        }
+        public bool DeleteEventFromCalendar()
+        {
+            return true;
+        }
+        public bool AddNewUsersToCalendar()
+        {
+            return true;
+        }
+        public bool DeleteUsersFromCalendar()
+        {
+            return true;
+        }
+        public bool AlterCalendarInformation()
+        {
+            return true;
+        }
+    }
+}
