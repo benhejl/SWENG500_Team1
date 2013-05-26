4a90c62180b911a38d263067df72dbe9682e6683
Ben Hejl <bzh128@psu.edu>
2013-05-26 14:12:26 -0400

    Add project model and business logic.

---
+++ ProjectManager/ProjectManagerLibrary/Models/Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectManagerBLL.Project
+{
+    class Project
+    {
+        public string Name { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+        public int Status { get; private set; }
+        public string Description { get; private set; }
+        public string Category { get; private set; }
+        public DateTime DueDate { get; private set; }
+
+        public Project(string name, string startDate, string endDate, int status, string description, string category, DateTime dueDate)
+        {
+        }
+
+        
+    }
+}
