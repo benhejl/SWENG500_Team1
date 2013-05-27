0a6b757542178b6b94cf4a62f21eef70b70287c0
Lauren <Laur10513@gmail.com>
2013-05-26 22:17:03 -0400

    Adding in Calendar and CalendarEvent objects and associated tests

---
+++ ProjectManager/ProjectManagerLibrary/Models/Calendar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectManagerLibrary.Models
+{
+    class Calendar
+    {
+        public string Name {get; set;}
+        public Project Project {get; set;}
+        public CalendarEvent[] Events {get; set;}
+        public User[] Users {get; set;}
+
+        public Calendar()
+        {
+            Name = null;
+            Project = null;
+            Events = null;
+            Users = null;
+        }
+
+        public Calendar(string Name, Project Project, CalendarEvent[] Events, User[] Users)
+        {
+            this.Name = Name;
+            this.Project = Project;
+            this.Events = Events;
+            this.Users = Users;
+        }
+    }
+}
---
+++ ProjectManager/ProjectManagerLibrary/Models/CalendarEvent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectManagerLibrary.Models
+{
+    class CalendarEvent
+    {
+        public enum EventType
+        {
+            MileStone,
+            Demo,
+            Vacation,
+            Task,
+            Sprint,
+            Custom
+        }
+
+        public string Name {get; set;}
+        public DateRange StartEndDate {get; set;}
+        public EventType Type {get; set;}
+
+        public CalendarEvent()
+        {
+            Name = null;
+            StartEndDate = null;
+        }
+
+        public CalendarEvent(string Name, DateRange StartEndDate, EventType Type)
+        {
+            this.Type = Type;
+            this.Name = Name;
+            this.StartEndDate = StartEndDate;
+        }
+    }
+}
