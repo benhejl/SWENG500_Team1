90316d5c1b1ad04f5e79599a4fb818eabfadc58a
Ben Hejl <bzh128@psu.edu>
2013-07-27 18:51:28 -0400

    Add IssuesByUserGraph unit tests.

--- ProjectManager/ProjectManagerTest/IssuesByUserGraphTest.cs
+++ ProjectManager/ProjectManagerTest/IssuesByUserGraphTest.cs
@@ -11,11 +11,11 @@ namespace ProjectManagerTest
     
     
     /// <summary>
-    ///This is a test class for IssuePriorityGraphTest and is intended
-    ///to contain all IssuePriorityGraphTest Unit Tests
+    ///This is a test class for IssuesByUserGraphTest and is intended
+    ///to contain all IssuesByUserGraphTest Unit Tests
     ///</summary>
     [TestClass()]
-    public class IssuePriorityGraphTest
+    public class IssuesByUserGraphTest
     {
 
 
@@ -69,13 +69,13 @@ namespace ProjectManagerTest
 
 
         /// <summary>
-        ///A test for IssuePriorityGraph Constructor
+        ///A test for IssuesByUserGraph Constructor
         ///</summary>
         [TestMethod()]
-        public void IssuePriorityGraphConstructorTest()
+        public void IssuesByUserGraphConstructorTest()
         {
-            IssuePriorityGraph target = new IssuePriorityGraph();
-            Assert.IsInstanceOfType(target, typeof(IssuePriorityGraph));
+            IssuesByUserGraph target = new IssuesByUserGraph();
+            Assert.IsInstanceOfType(target, typeof(IssuesByUserGraph));
         }
 
         /// <summary>
@@ -85,7 +85,7 @@ namespace ProjectManagerTest
         [DeploymentItem("ProjectManagerLibrary.dll")]
         public void CreateChartAreaTest()
         {
-            IssuePriorityGraph_Accessor target = new IssuePriorityGraph_Accessor();
+            IssuesByUserGraph_Accessor target = new IssuesByUserGraph_Accessor(); // TODO: Initialize to an appropriate value
             ChartArea actual = target.CreateChartArea();
             Assert.AreEqual(true, actual.Area3DStyle.Enable3D);
         }
@@ -96,20 +96,16 @@ namespace ProjectManagerTest
         [TestMethod()]
         public void DisplayTest()
         {
-            IssuePriorityGraph target = new IssuePriorityGraph(); // TODO: Initialize to an appropriate value
-            Project project = BuildDefaultProject();
+            IssuesByUserGraph target = new IssuesByUserGraph();
+            Project project = new Project(1, "Test", DateTime.Now, DateTime.Now.AddDays(1), "", "", "", DateTime.Now.AddDays(1));
             DateRange range = new DateRange(DateTime.Now.Subtract(TimeSpan.FromDays(10)), DateTime.Now);
-            AddIssues(project, 8, 2, 0);
 
             Control actual = target.Display(project, range);
             Assert.IsInstanceOfType(actual, typeof(Chart));
 
             Chart chart = actual as Chart;
             Assert.AreEqual(1, chart.Series.Count);
-            Assert.AreEqual(3, chart.Series[0].Points.Count);
-            Assert.AreEqual(8, chart.Series[0].Points[0].YValues[0]);
-            Assert.AreEqual(2, chart.Series[0].Points[1].YValues[0]);
-            Assert.AreEqual(0, chart.Series[0].Points[2].YValues[0]);
+            Assert.AreEqual(0, chart.Series[0].Points.Count);
             Assert.AreEqual(1, chart.Legends.Count);
             Assert.AreEqual(1, chart.Titles.Count);
         }
@@ -121,22 +117,12 @@ namespace ProjectManagerTest
         [DeploymentItem("ProjectManagerLibrary.dll")]
         public void EvaluateProjectTest()
         {
-            IssuePriorityGraph_Accessor target = new IssuePriorityGraph_Accessor();
-            Project project = BuildDefaultProject();
+            IssuesByUserGraph_Accessor target = new IssuesByUserGraph_Accessor(); // TODO: Initialize to an appropriate value
+            Project project = new Project(1, "Test", DateTime.Now, DateTime.Now.AddDays(1), "", "", "", DateTime.Now.AddDays(1));
             DateRange range = new DateRange(DateTime.Now, DateTime.Now);
             List<Series> actual = target.EvaluateProject(project, range);
             Assert.AreEqual(1, actual.Count);
-            Assert.AreEqual(3, actual[0].Points.Count);
-            Assert.AreEqual(0, actual[0].Points[0].YValues[0]);
-            Assert.AreEqual(0, actual[0].Points[1].YValues[0]);
-            Assert.AreEqual(0, actual[0].Points[1].YValues[0]);
-
-            AddIssues(project, 3, 7, 2);
-            actual = target.EvaluateProject(project, range);
-
-            Assert.AreEqual(3, actual[0].Points[0].YValues[0]);
-            Assert.AreEqual(7, actual[0].Points[1].YValues[0]);
-            Assert.AreEqual(2, actual[0].Points[2].YValues[0]);
+            Assert.AreEqual(0, actual[0].Points.Count);
         }
 
         /// <summary>
@@ -146,7 +132,7 @@ namespace ProjectManagerTest
         [DeploymentItem("ProjectManagerLibrary.dll")]
         public void CurrentDateRangeTest()
         {
-            IssuePriorityGraph_Accessor target = new IssuePriorityGraph_Accessor();
+            IssuesByUserGraph_Accessor target = new IssuesByUserGraph_Accessor();
             Assert.AreEqual(DateTime.MinValue, target.CurrentDateRange.StartTime);
             Assert.AreEqual(DateTime.MaxValue, target.CurrentDateRange.FinishTime);
         }
@@ -157,8 +143,8 @@ namespace ProjectManagerTest
         [TestMethod()]
         public void DataTitleTest()
         {
-            IssuePriorityGraph target = new IssuePriorityGraph();
-            Assert.AreEqual("Issues By Priority", target.DataTitle);
+            IssuesByUserGraph target = new IssuesByUserGraph();
+            Assert.AreEqual("Issues By User", target.DataTitle);
         }
 
         /// <summary>
@@ -167,7 +153,7 @@ namespace ProjectManagerTest
         [TestMethod()]
         public void RequiresDateRangeTest()
         {
-            IssuePriorityGraph target = new IssuePriorityGraph();
+            IssuesByUserGraph target = new IssuesByUserGraph();
             Assert.AreEqual(false, target.RequiresDateRange);
         }
 
@@ -177,30 +163,8 @@ namespace ProjectManagerTest
         [TestMethod()]
         public void SortOrderTest()
         {
-            IssuePriorityGraph target = new IssuePriorityGraph();
+            IssuesByUserGraph target = new IssuesByUserGraph();
             Assert.AreEqual(1, target.SortOrder);
-        }
-
-        private Project BuildDefaultProject()
-        {
-            return new Project(1, "Test", DateTime.Now, DateTime.Now, "Status", "Description", "Category", DateTime.Now);
-        }
-
-        private void AddIssues(Project project, int high, int medium, int low)
-        {
-            AddIssuePriorityCount(project, high, Issue.IssuePriority.High);
-            AddIssuePriorityCount(project, medium, Issue.IssuePriority.Medium);
-            AddIssuePriorityCount(project, low, Issue.IssuePriority.Low);
-        }
-
-        private static void AddIssuePriorityCount(Project project, int count, Issue.IssuePriority priority)
-        {
-            for (int i = 0; i < count; i++)
-            {
-                Issue issue = new Issue();
-                issue.CurrentPriority = priority;
-                project.AddIssue(issue);
-            }
         }
     }
 }
